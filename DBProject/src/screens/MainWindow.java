e8796ff0a15c2ccbf28efe8dd4e68e14eda421f5
Giannandrea <34889642+Giannandrea-Vicidomini@users.noreply.github.com>
2021-03-13 18:17:02 +0100

    Update MainWindow.java

--- DBProject/src/screens/MainWindow.java
+++ DBProject/src/screens/MainWindow.java
@@ -145,14 +145,8 @@ public class MainWindow {
         if(dbInfo.getDbName().equals("steamDB")) {
         	
         	queryMenu = new JMenu("Default Queries");
-            /*
-             * 
-             * 
-             * 
-             *     ADDING QUERIES HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-             * 
-             * 
-             */
+            
+             
         	JMenuItem updateQueryMenu = new JMenuItem("Add Account");
         	updateQueryMenu.setForeground(new Color(30, 144, 255));
         	updateQueryMenu.setBackground(new Color(255, 255, 255));
